4d25044e4f2fe539ae6aa4746c3431dcd12b7982
Gautam <gnayar33@gmail.com>
2013-03-04 18:25:59 -0500

    Transparent Layer

--- src/com/example/weather/MainActivity.java
+++ src/com/example/weather/MainActivity.java
@@ -8,28 +8,28 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.view.GestureDetectorCompat;
 import android.support.v4.view.MotionEventCompat;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	private static final String DEBUG_TAG = "Motion"; 
-	private GestureDetectorCompat mDetector;
-	private ArrayList<Integer> buffer = new ArrayList<Integer>();
 	int sum = 0;
 	int color = 0;
 	
-	RelativeLayout stacker;
-	RelativeLayout surface;
+	ViewGroup stacker;
+	RelativeLayout touchlayer;
+	RelativeLayout weather;
+	TextView statusbox;
 	ListView hours;
 	Context context;
 	int screenHeight, screenWidth;
@@ -50,7 +50,14 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		stacker = (RelativeLayout)findViewById(R.id.main);
+		touchlayer = new RelativeLayout(this);
+		stacker.addView(touchlayer);
+		stacker.bringChildToFront(touchlayer);
+		touchlayer.setEnabled(true);
+		
 		context = this;
+		
+		constructBaseLevel();
 		
 		state = State.IDLE;
 		previousState = State.IDLE;
@@ -110,10 +117,9 @@ public class MainActivity extends Activity {
 		
 		Log.v("db", "Inserting...");
 		
-		Comment comment = null;
-		comment = datasource.createComment("TEST");
-		comment = datasource.createComment("TEST1");
-		comment = datasource.createComment("TEST2");
+		datasource.createComment("TEST");
+		datasource.createComment("TEST1");
+		datasource.createComment("TEST2");
 
 
 		
@@ -127,188 +133,147 @@ public class MainActivity extends Activity {
 			String log = "Name: " + comments.get(i).toString();
 			Log.v("db", log);
 		}
-
-
-
-
-		mDetector = new GestureDetectorCompat(this, new MyGestureListener());
+		
 	}
-
+	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		this.mDetector.onTouchEvent(event);
-		RelativeLayout main = (RelativeLayout)findViewById(R.id.surface);
 
 		int action = MotionEventCompat.getActionMasked(event);
-        
-	    switch(action) {
-	        case (MotionEvent.ACTION_DOWN) :
-	        	int x = (int) event.getX();
-	        	int y = (int) event.getY();
-	            //Log.v("state","Action was DOWN: x -> " + x + " y -> " + y);
 
-		        if(x < screenWidth/2 && y < screenHeight/2) {     
-		        	previousState = State.Q2;
-	            
-	            } else if (x > screenWidth/2 && y < screenHeight/2) {
-	            	previousState = State.Q1;
-	            } else if (x < screenWidth/2 && y > screenHeight/2) {
-	            	previousState = State.Q3;
-	            } else if (x > screenWidth/2 && y > screenHeight/2) {
-	            	previousState = State.Q4;
-	            }
-		       // Log.v("state", "previous state: " + previousState.toString());
-		        return true;
-	        
-	        case (MotionEvent.ACTION_MOVE) :
+		switch (action) {
+		case (MotionEvent.ACTION_DOWN):
+			int x = (int) event.getX();
+			int y = (int) event.getY();
+			Log.d(DEBUG_TAG,"Action was DOWN: x -> " + x + " y -> " + y);
 
-	            //Log.d(DEBUG_TAG,"Action was MOVE");
-	                        
-	            
-	            return true;
-	        case (MotionEvent.ACTION_UP) :
-	            //Log.d(DEBUG_TAG,"Action was UP");
-	        
-        		int x1 = (int) event.getX();
-        		int y1 = (int) event.getY();
-		        if(x1 < screenWidth/2 && y1 < screenHeight/2) {
-	            	state = State.Q2;            
-	            } else if (x1 > screenWidth/2 && y1 < screenHeight/2) {
-	            	state = State.Q1;
-	            } else if (x1 < screenWidth/2 && y1 > screenHeight/2) {
-	            	state = State.Q3;
-	            } else if (x1 > screenWidth/2 && y1 > screenHeight/2) {
-	            	state = State.Q4;
-	            }
+			if (x < screenWidth / 2 && y < screenHeight / 2) {
+				previousState = State.Q2;
 
-		        
-	            //now we have two states to handle...one is the state we are coming from and the other is the state we currently
-	            //moved to...
-	            //now do the logic 
-	            
-	        
-		        
-	            Log.v("state", "Previous state: " + previousState.toString() + " Current state: " + state.toString());
-	            if(previousState == State.Q2 && state == State.Q3) {
-	            	Log.v("state", "Left pull down");
-	            	if(constructListView()) {
-	            		Log.v("state", "List view returned true");
-	            	} else 
-	            		Log.v("state", "List view returned false");
-	            	
-	        		((TextView)findViewById(R.id.text)).setText("Left Down");
-	            } else if (previousState == State.Q3 && state == State.Q2) {
-	        		((TextView)findViewById(R.id.text)).setText("Left Up");
-	            	Log.v("state", "Left pull up");
-	            } else if (previousState == State.Q1 && state == State.Q4) {
-	        		((TextView)findViewById(R.id.text)).setText("Right Down");
-	            	Log.v("state", "Right pull down");
-	            } else if (previousState == State.Q4 && state == State.Q1) {
-	        		((TextView)findViewById(R.id.text)).setText("Right Up");
+			} else if (x > screenWidth / 2 && y < screenHeight / 2) {
+				previousState = State.Q1;
+			} else if (x < screenWidth / 2 && y > screenHeight / 2) {
+				previousState = State.Q3;
+			} else if (x > screenWidth / 2 && y > screenHeight / 2) {
+				previousState = State.Q4;
+			}
+			// Log.v("state", "previous state: " + previousState.toString());
+			return true;
 
-	            	Log.v("state", "Right pull up");
-	            }
-	        
-	            
-	            
-	            
-		        if(listViewUp == true){
-		        	surface = (RelativeLayout)findViewById(R.id.surface);
-	        		stacker.bringChildToFront(surface);
-	        		Log.d(DEBUG_TAG,"UP with remove"); 
-	        		//listViewUp = false;
-	        	} 
-            	return true;
-	        case (MotionEvent.ACTION_CANCEL) :
-	            Log.d(DEBUG_TAG,"Action was CANCEL");
-	            return true;
-	        case (MotionEvent.ACTION_OUTSIDE) :
-	            Log.d(DEBUG_TAG,"Movement occurred outside bounds " +
-	                    "of current screen element");
-	            return true;      
-	        default : 
-	            return super.onTouchEvent(event);
-	    }      
-		
+		case (MotionEvent.ACTION_MOVE):
+
+			Log.d(DEBUG_TAG,"Action was MOVE");
+			int x1 = (int) event.getX();
+			int y1 = (int) event.getY();
+			if (x1 < screenWidth / 2 && y1 < screenHeight / 2) {
+				state = State.Q2;
+			} else if (x1 > screenWidth / 2 && y1 < screenHeight / 2) {
+				state = State.Q1;
+			} else if (x1 < screenWidth / 2 && y1 > screenHeight / 2) {
+				state = State.Q3;
+			} else if (x1 > screenWidth / 2 && y1 > screenHeight / 2) {
+				state = State.Q4;
+			}
+
+			// now we have two states to handle...one is the state we are coming
+			// from and the other is the state we currently
+			// moved to...
+			// now do the logic
+
+			Log.v("state", "Previous state: " + previousState.toString()
+					+ " Current state: " + state.toString());
+			if (previousState == State.Q2 && state == State.Q3) {
+				Log.v("state", "Left pull down");
+				if (constructListView()) {
+					Log.v("state", "List view returned true");
+				} else
+					Log.v("state", "List view returned false");
+
+				//((TextView) findViewById(R.id.text)).setText("Left Down");
+			} else if (previousState == State.Q3 && state == State.Q2) {
+				//((TextView) findViewById(R.id.text)).setText("Left Up");
+				Log.v("state", "Left pull up");
+			} else if (previousState == State.Q1 && state == State.Q4) {
+				//((TextView) findViewById(R.id.text)).setText("Right Down");
+				Log.v("state", "Right pull down");
+			} else if (previousState == State.Q4 && state == State.Q1) {
+				//((TextView) findViewById(R.id.text)).setText("Right Up");
+
+				Log.v("state", "Right pull up");
+			}
+
+			return true;
+		case (MotionEvent.ACTION_UP):
+			// Log.d(DEBUG_TAG,"Action was UP");
+			if (listViewUp == true) {
+				stacker.bringChildToFront(weather);
+				stacker.bringChildToFront(touchlayer);
+				touchlayer.setEnabled(true);
+				Log.d(DEBUG_TAG, "UP with remove");
+				listViewUp = false;
+			}
+			return true;
+		case (MotionEvent.ACTION_CANCEL):
+			Log.d(DEBUG_TAG, "Action was CANCEL");
+			return true;
+		case (MotionEvent.ACTION_OUTSIDE):
+			Log.d(DEBUG_TAG, "Movement occurred outside bounds "
+					+ "of current screen element");
+			return true;
+		default:
+			return super.onTouchEvent(event);
+		}
+
 	}
 
 	public boolean constructListView() {
 		//main.setBackgroundColor(Color.LTGRAY);
+		if(listViewUp==false){
+			hours = new ListView(context);
+			
+			String[] stringArray = new String[] { "1", "2","3","4","5","6","7","8","9","10","11","12" };
+			ArrayAdapter<String> modeAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, android.R.id.text1, stringArray);
+			hours.setAdapter(modeAdapter);
+			stacker.addView(hours);
+			
+			hours.setEnabled(false);
+			hours.setBackgroundColor(0xFF0099cc);
+			listViewUp = true;
+			stacker.bringChildToFront(hours);
+			stacker.bringChildToFront(touchlayer);
+			touchlayer.setEnabled(true);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean constructBaseLevel(){
 		
-		hours = new ListView(context);
+		statusbox = new TextView(context);
+		statusbox.setLayoutParams(new LayoutParams(
+	            LayoutParams.FILL_PARENT,
+	            LayoutParams.WRAP_CONTENT));
+		statusbox.setText("Swipper");
 		
-		String[] stringArray = new String[] { "1", "2","3","4","5","6","7","8","9","10","11","12" };
-		ArrayAdapter<String> modeAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, android.R.id.text1, stringArray);
-		hours.setAdapter(modeAdapter);
-		stacker.addView(hours);
+		weather = new RelativeLayout(context);
+		weather.setLayoutParams(new LayoutParams(
+	            LayoutParams.FILL_PARENT,
+	            LayoutParams.FILL_PARENT));
+		weather.setBackgroundColor(0xFFff8800);
 		
-		listViewUp = true;
-		stacker.bringChildToFront(hours);
+		weather.addView(statusbox);
+		weather.bringChildToFront(statusbox);
+		
+		stacker.addView(weather);
+		stacker.bringChildToFront(weather);
+		stacker.bringChildToFront(touchlayer);
+		touchlayer.setEnabled(true);
+		
+		
 		return true;
 	}
 	
 	
-	 class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-	        
-	        
-//	        @Override
-//	        public boolean onDown(MotionEvent event) { 
-//	            Log.d(DEBUG_TAG,"onDown: "); 
-//	        	buffer.clear();
-//	            return true;
-//	        }
-//	        
-//
-//	        @Override
-//	        public boolean onScroll(MotionEvent event1, MotionEvent event2, 
-//	                float distanceX, float distanceY) {
-//	        	
-//	        	int middle = (((RelativeLayout)findViewById(R.id.main)).getRight())/2;
-//	        	
-//	        	buffer.add((int)distanceY);
-//	        	if(buffer.size()>20){
-//	        		buffer.remove(0);
-//	        	}
-//	        	sum = 0;
-//	        	for(int i = 0;i<buffer.size();i++){
-//	        		sum+= buffer.get(i);
-//	        	}
-//	        	boolean swiped = false;
-//	        	 
-//        		RelativeLayout main = (RelativeLayout)findViewById(R.id.surface);
-//	        	if((event1.getRawX()<middle)&&(sum>400)){
-//	        		swiped = true;
-//	        		main.setBackgroundColor(Color.GRAY);
-//	        		((TextView)findViewById(R.id.text)).setText("Left Up");
-//	        	}
-//	        	else if((event1.getRawX()>middle)&&(sum>400)){
-//	        		swiped = true;
-//	        		main.setBackgroundColor(Color.GRAY);
-//	        		((TextView)findViewById(R.id.text)).setText("Right Up");
-//	        	}
-//	        	else if((event1.getRawX()<middle)&&(sum<-400)){
-//	        		swiped = true;
-//	        		main.setBackgroundColor(Color.LTGRAY);
-//	        		
-//	        		hours = new ListView(context);
-//	        		
-//	        		String[] stringArray = new String[] { "1", "2","3","4","5","6","7","8","9","10","11","12" };
-//	        		ArrayAdapter<String> modeAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, android.R.id.text1, stringArray);
-//	        		hours.setAdapter(modeAdapter);
-//	        		stacker.addView(hours);
-//	        		
-//	        		listViewUp = true;
-//	        		stacker.bringChildToFront(hours);
-//	        	}
-//	        	else if((event1.getRawX()>middle)&&(sum<-400)){
-//	        		swiped = true;
-//	        		main.setBackgroundColor(Color.LTGRAY);
-//	        		((TextView)findViewById(R.id.text)).setText("Right Down");
-//	        	}
-//	        	
-//	           // Log.d(DEBUG_TAG, "onScroll: "+sum);
-//	            return true;
-//	        }
-	    }
+	 
 }
 
